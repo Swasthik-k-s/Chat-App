82167569dee4ab1f744f884277a991441bfb7064
Swasthik <62685591+Swasthik-k-s@users.noreply.github.com>
2021-11-20 15:01:37 +0530

    Add files via upload

---
+++ Chat App/Utilities/Protocols.swift
@@ -0,0 +1,14 @@
+//
+//  Protocols.swift
+//  Chat App
+//
+//  Created by Swasthik K S on 16/11/21.
+//
+
+protocol UserAuthenticatedDelegate {
+    func userAuthenticated()
+}
+
+protocol ChatSelectedDelegate {
+    func chatSelected(isSelected: Bool)
+}
